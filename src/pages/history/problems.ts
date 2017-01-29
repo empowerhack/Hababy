cbb60a1062d5d14ca83c010433db6a17c97a4bdd
Derek Chung <derek@Dereks-MacBook-Pro.local>
2017-01-29 16:10:25 -0500

    start medical problems form

--- src/pages/history/problems.ts
+++ src/pages/history/problems.ts
@@ -6,6 +6,27 @@ import {TranslateService } from 'ng2-translate/ng2-translate';
 })
 export class HistoryProblemsPage {
 
+  cesarian:
+   { had?: boolean,
+     smallbaby?: boolean } = {};
+  wassmall: boolean;
+  stuckduringdelivery: boolean;
+  heavybleeding: boolean;
+  liverproblems: boolean;
+  highbloodsugar: boolean;
+  bloodclots: boolean;
+  deliveredearly: boolean;
+  highpressure: boolean;
+
+
   constructor( private translate: TranslateService ) {
+  }
+
+  onCesarian() {
+    console.log("cesarian change: " + this.cesarian.had);
+  }
+
+  saveAndContinue() {
+    console.log("cesarian: " + this.cesarian.had);
   }
 }
